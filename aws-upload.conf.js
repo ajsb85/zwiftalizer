02af6538ded35352b129514dc90acc816e168a30
mhanney <mrh5937@gmail.com>
2016-08-24 22:01:37 -0700

    added screenies

---
+++ aws-upload.conf.js
@@ -0,0 +1,13 @@
+module.exports = {
+  credentials: 'aws-credentials.json',
+  bucketName: 'zwiftalizer.com',
+  patterns: [
+    'public/img/*.*',
+    'public/bootstrap/*.*',
+    'public/fonts/*.*',
+    'public/bundle.js',
+    'public/favicon.ico',
+    'public/demolog.txt',
+    'public/index.html'
+  ]
+};
